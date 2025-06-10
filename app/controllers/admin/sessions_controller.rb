e83e453e9e638a75ecdfce5cd7c848caaf505df1
Harrison Blake <harrison.r.blake@gmail.com>
2025-06-10 16:09:03 -0400

    add navbar

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,12 +1,16 @@
 class Admin::SessionsController < ApplicationController
-  def new; end
+  def new
+    if session[:admin_id].present?
+      redirect_to admin_appointments_path
+    end
+  end
 
   def create
     user = User.find_by(email: params[:email])
 
     if user&.authenticate(params[:password]) && user.admin?
       session[:admin_user_id] = user.id
-      redirect_to admin_dashboard_path, notice: "Logged in as admin."
+      redirect_to admin_appointments_path, notice: "Logged in as admin."
     else
       flash.now[:alert] = "Invalid credentials or not an admin"
       render :new
